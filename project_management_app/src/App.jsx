257b2ca21dd45640112f0b5de717988e1594ea67
DenysPostolnyi <postlden44@gmail.com>
2023-10-30 10:34:13 +0200

    starting project management project

---
+++ project_management_app/src/App.jsx
@@ -0,0 +1,13 @@
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+
+function App() {
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectSidebar/>
+            <NewProject/>
+        </main>
+    );
+}
+
+export default App;
