2b3b7c726e1b7fdc25d4b48cdd579d8b60b81875
DenysPostolnyi <postlden44@gmail.com>
2023-12-26 13:11:25 +0200

    updates

--- project_management_app/src/App.jsx
+++ project_management_app/src/App.jsx
@@ -1,12 +1,102 @@
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from 'react';
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectID: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectID: null,
+            };
+        });
+    }
+
+    function handleAddTask(task) {
+        setProjectsState(prevState => {
+            const newTask = {
+                text: task,
+                projectID: prevState.selectedProjectID,
+                id: Math.random()
+            };
+            return {
+                ...prevState,
+                tasks: [newTask, ...prevState.tasks]
+            };
+        });
+    }
+
+    function handleDeleteTask() {
+    }
+
+    const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectID);
+
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}
+                                   onDeleteTask={handleDeleteTask} onAddTask={handleAddTask}/>;
+
+    if (projectsState.selectedProjectID === null) {
+        content = <NewProject onCancel={handleCancel} onAddProject={handleAddProject}/>;
+    } else if (projectsState.selectedProjectID === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+    }
+
+    function handleAddProject(projectData) {
+        setProjectsState(prevState => {
+            const newProject = {
+                ...projectData,
+                id: Math.random()
+            };
+            return {
+                ...prevState,
+                selectedProjectID: undefined,
+                projects: [...prevState.projects, newProject]
+            };
+        });
+    }
+
+    function handleCancel() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectID: undefined,
+            };
+        });
+    }
+
+    function handleSelectProject(id) {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectID: id,
+            };
+        });
+    }
+
+    function handleDeleteProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectID: undefined,
+                projects: prevState.projects.filter((project) => {
+                    project.id !== prevState.selectedProjectID
+                })
+            };
+        });
+    }
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectSidebar/>
-            <NoProjectSelected/>
+            <ProjectSidebar onStartAddProject={handleStartAddProject}
+                            projects={projectsState.projects}
+                            onSelectProject={handleSelectProject}/>
+            {content}
         </main>
     );
 }
---
+++ project_management_app/src/components/Tasks.jsx
@@ -0,0 +1,16 @@
+import NewTask from "./NewTask.jsx";
+
+export default function Tasks({tasks, onAdd, onDelete}) {
+    return <section>
+        <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+        <NewTask onAdd={onAdd} onDelete={onDelete}/>
+        {tasks.length === 0 && <p className="text-stone-800 my-4">This project does not have any tasks yet.</p>}
+        {tasks.length > 0 && <ul>
+            {tasks.map((task)=>(
+                <li key={task.id}>
+                    <span>{task.text}</span>
+                </li>
+            ))}
+        </ul>}
+    </section>
+}
